a5b78eb8d8654d0af8d29257a242b6bb004a36e3
Aditi-Bari14 <aditi.bari2004@gmail.com>
2026-01-12 12:29:06 +0530

    Routes separated

--- backend/app.py
+++ backend/app.py
@@ -9,22 +9,6 @@ ML_SERVICE_URL = "http://127.0.0.1:8000"
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@app.route("/api/history", methods=["GET"])
-def get_history():
-    try:
-        with open("history.json", "r") as f:
-            history = json.load(f)
-    except FileNotFoundError:
-        history = []
-
-    history = sorted(history, key=lambda x: x["date"], reverse=True)
-    return jsonify(history)
-
-history_record = {
-    "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-    "prediction": result["prediction"],
-    "confidence": round(result["confidence"] * 100, 2)
-}
 # -------------------------
 # PREDICT (Frontend → Backend → ML)
 # -------------------------
@@ -51,4 +35,4 @@ def get_history():
 
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(port=5000, debug=True)
